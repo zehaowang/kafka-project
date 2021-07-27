44b09000ff88a8ea9227cee8037af0a204c6d361
zehaowang <zw2323@columbia.edu>
2021-07-27 00:05:33 -0400

    first commit

---
+++ src/main/java/com/github/zehaowang/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,35 @@
+package com.github.zehaowang.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+        // create Producer properties
+        String bootstrapServers = "127.0.0.1:9092";
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a producer record
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello world");
+
+        // send data - asynchronous
+        producer.send(record);
+
+        //flush
+        producer.flush();
+
+        //flush and close
+        producer.close();
+    }
+}
